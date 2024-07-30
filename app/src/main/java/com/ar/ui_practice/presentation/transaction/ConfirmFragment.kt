051fe25f95886d8d701c6dbde070b1bf90262e40
Md Asfakur Rahat <asfakur.rahat@brainstation-23.com>
2024-07-30 17:21:28 +0600

    Add all logical things to ConfirmPIN and Success fragment

--- app/src/main/java/com/ar/ui_practice/presentation/transaction/ConfirmFragment.kt
+++ app/src/main/java/com/ar/ui_practice/presentation/transaction/ConfirmFragment.kt
@@ -1,5 +1,6 @@
 package com.ar.ui_practice.presentation.transaction
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.text.Editable
@@ -9,15 +10,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ar.ui_practice.adapter.transaction.PinAdapter
 import com.ar.ui_practice.databinding.FragmentConfirmBinding
+import com.google.android.material.internal.ViewUtils.hideKeyboard
 
 class ConfirmFragment : Fragment() {
     private var _binding : FragmentConfirmBinding? = null
     private val binding get() = _binding!!
     private lateinit var adapter: PinAdapter
+
+    private val args: ConfirmFragmentArgs by navArgs()
 
 
     override fun onCreateView(
@@ -42,20 +48,25 @@ class ConfirmFragment : Fragment() {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
             }
-
+            @SuppressLint("RestrictedApi")
             override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 val len = s?.length ?: 0
-                val list = mutableListOf<Int>()
-                for(i in 0..<len){
-                    list.add(i)
-                }
-                setUpPIN(list)
-            }
+                if (len <= 4){
+                    val list = mutableListOf<Int>()
+                    for(i in 0..<len){
+                        list.add(i)
+                    }
+                    setUpPIN(list)
 
+                    if (len == 4){
+                        hideKeyboard(binding.root)
+                        binding.etPIN.clearFocus()
+                    }
+                }
+            }
             override fun afterTextChanged(p0: Editable?) {
 
             }
-
         })
     }
 
@@ -69,12 +80,38 @@ class ConfirmFragment : Fragment() {
 
     private fun initView() {
         binding.actionBar.tvNavTitle.text = "Mobile Recharge"
+        binding.contactName.text = args.name
+        binding.contactNumber.text = args.number
+        binding.tvAmount.text = args.amount
     }
 
     private fun initListener() {
         binding.actionBar.ivNavIcon.setOnClickListener { 
             findNavController().popBackStack()
         }
+
+        binding.slider.onSlideCompleteListener = {
+            if(binding.etPIN.text.toString().trimMargin() == "1111"){
+                gotoSuccess()
+            }else{
+                gotoFailure()
+            }
+        }
+
+        binding.etPIN.setOnClickListener {
+            binding.etPIN.setText("")
+            setUpPIN(mutableListOf())
+        }
+    }
+
+    private fun gotoFailure() {
+        Toast.makeText(requireContext(), "Please Enter the correct PIN", Toast.LENGTH_SHORT).show()
+        binding.etPIN.setText("")
+        setUpPIN(mutableListOf())
+    }
+
+    private fun gotoSuccess() {
+        findNavController().navigate(ConfirmFragmentDirections.actionConfirmFragmentToSuccessFragment(name = args.name, number = args.number, amount = args.amount, charge = binding.tvCharge.text.toString()))
     }
 
 
--- app/src/main/java/com/ar/ui_practice/presentation/transaction/SuccessFragment.kt
+++ app/src/main/java/com/ar/ui_practice/presentation/transaction/SuccessFragment.kt
@@ -1,9 +1,78 @@
 package com.ar.ui_practice.presentation.transaction
 
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.ar.ui_practice.R
+import com.ar.ui_practice.databinding.FragmentConfirmBinding
+import com.ar.ui_practice.databinding.FragmentSuccessBinding
+import java.security.SecureRandom
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class SuccessFragment : Fragment() {
 
+    private var _binding : FragmentSuccessBinding? = null
+    private val binding get() = _binding!!
+    private val args: SuccessFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSuccessBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.btnBackHome.setOnClickListener {
+            findNavController().popBackStack(R.id.homeFragment, false)
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun initView() {
+        binding.contactName.text = args.name
+        binding.contactNumber.text = args.number
+        binding.transactionAmount.text = args.amount
+        binding.tvCharge.text = args.charge
+        binding.totalAmount.text = (args.amount.toInt() + if(args.charge == "Free") 0 else 10).toString()
+        binding.currentBalance.text = (400000 - (args.amount.toInt() + if(args.charge == "Free") 0 else 10)).toString()
+        setDate()
+        binding.tvTransactionId.text = generateTransactionID(21)
+    }
+
+    private fun setDate() {
+        val date = Date()
+        val pattern = "hh:mm a\ndd MMM yyyy"
+        val simpleDateFormat = SimpleDateFormat(pattern, Locale.getDefault())
+        binding.tvTimeDate.text =  simpleDateFormat.format(date)
+    }
+
+    fun generateTransactionID(length: Int): String {
+        val charPool = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        val random = SecureRandom()
+        val stringBuilder = StringBuilder()
+
+        repeat(length) {
+            val index = random.nextInt(charPool.length)
+            stringBuilder.append(charPool[index])
+        }
+
+        return stringBuilder.toString()
+    }
 }
